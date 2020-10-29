0ffa620a79275a7e8c563d9ec76112ec1e84381c
lislmaka <70829116+lislmaka@users.noreply.github.com>
2020-10-29 13:17:58 +0300

    new func

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js');
-mix.sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css').options({
+    processCssUrls: false
+});
 
 mix.copyDirectory('resources/images', 'public/images');
 
