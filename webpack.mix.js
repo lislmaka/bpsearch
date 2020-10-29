40f62494aa320c7c29b0abb134a1d65ae53b2979
lislmaka <70829116+lislmaka@users.noreply.github.com>
2020-10-29 14:10:40 +0300

    fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js');
 mix.sass('resources/sass/app.scss', 'public/css').options({
-    processCssUrls: true
+    processCssUrls: false
 });
 
 mix.copyDirectory('resources/images', 'public/images');
